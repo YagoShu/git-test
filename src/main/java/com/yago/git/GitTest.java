0a1fba396e4b5d826da48f76525793726dbaeabc
Yagoshu <2929160515@qq.com>
2023-02-04 10:25:48 +0000

    update src/main/java/com/yago/git/GitTest.java.
    Signed-off-by: Yagoshu <2929160515@qq.com>

--- src/main/java/com/yago/git/GitTest.java
+++ src/main/java/com/yago/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("push test");
         System.out.println("pull test");
         System.out.println("gitee push test");
+        System.out.println("gitee pull test");
     }
 
 }
